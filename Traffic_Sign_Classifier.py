99880f8d0837a6666a67b36941a10de3a1be1202
x65han <x65han@uwaterloo.ca>
2018-02-01 07:42:59 -0500

    Phase 2: Visualize Data Set

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -24,5 +24,42 @@ print("X_valid:", X_valid.shape)
 print("y_valid:", y_valid.shape)
 print("X_test:", X_test.shape)
 print("y_test:", y_test.shape)
+print("Sample input shape:", X_train[0].shape)
+print("Sample output:", y_train[0])
 
-print(y_train[0])
+# Dataset Summary & Exploration
+import numpy as np
+# Number of training examples
+n_train = len(X_train)
+# Number of testing examples
+n_test = len(X_test)
+# shape of input image
+image_shape = X_train[0].shape
+# Number of unique labels
+n_classes = len(np.unique(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Shape of image data =", image_shape)
+print("Number of classes =", n_classes)
+
+# Visualize data
+import matplotlib.pyplot as plt
+import random
+
+def visualize_data():
+    fig, axs = plt.subplots(3, 5, figsize=(15, 6))
+    fig.subplots_adjust(hspace = .2, wspace=.001)
+    axs = axs.ravel()
+
+    for i in range(15):
+        index = random.randint(0, len(X_train) - 1)
+        image = X_train[index]
+        axs[i].axis('off')
+        axs[i].imshow(image)
+        axs[i].set_title(y_train[index])
+    plt.show()
+
+visualize_data()
+
+
